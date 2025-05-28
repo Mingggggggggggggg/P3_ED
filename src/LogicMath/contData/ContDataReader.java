ca0d479ac23e1bc1c9f4a993d4011366c0e249c7
Ming <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-05-28 12:50:53 +0200

    123

--- src/LogicMath/contData/ContDataReader.java
+++ src/LogicMath/contData/ContDataReader.java
@@ -72,5 +72,69 @@ public class ContDataReader {
         System.arraycopy(data, 0, result, 0, dataLength);
         return result;
     }
-}
 
+public static double[] getContClasses(String name) throws IOException {
+        System.out.println(name);
+        double[] data = new double[100];
+        int dataLength = 0;
+        double[] result;
+        FileReader in = null;
+        BufferedReader br = null;
+        String s = "";
+        String[] select;
+
+        try {
+            in = new FileReader(name);
+            br = new BufferedReader(in);
+            
+            s = br.readLine().trim();
+            //System.out.println(s);
+
+            if (!s.equals("contdata")) {
+                System.err.println("Dateiformat ungueltig");
+                if (s.equals("discdata")) {
+                    System.out.println("Falsche Datei ausgewählt. Meinten Sie contdata.p3?");
+                    System.exit(-1);
+                }
+                System.exit(-1);
+            } 
+
+            while ((s = br.readLine()) != null) {
+                s = s.trim();
+
+                if (s.equals("class")) {
+
+                    s = s.trim();
+                    while ((s = br.readLine()) != null && !s.equals("endclass")) {
+                        select = s.split(";");
+
+                        for (int i = 0; i < select.length; i++) {
+                            data[i] = Double.parseDouble(select[i].trim());
+                            dataLength++;
+                        }
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Datei nicht gefunden.");
+            System.err.println(e);
+        } catch (IOException ex) {
+            System.err.println("Datei konnte nicht gelesen werden.");
+            System.err.println(ex);
+        } finally {
+            if (br != null) {
+                br.close();
+            }
+            if (in != null) {
+                in.close();
+            }
+        }
+
+        // Kopiere data-Array in ein result-array mit der korrekten Länge.
+        //System.out.println(dataLength);
+        result = new double[dataLength];
+        System.arraycopy(data, 0, result, 0, dataLength);
+        return result;
+    }
+}
