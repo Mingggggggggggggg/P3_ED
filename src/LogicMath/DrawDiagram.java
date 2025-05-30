8eacf17a993fa0563056d0f3843d57ca564a980d
Ming <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-05-30 11:05:25 +0200

    5641

---
+++ src/LogicMath/DrawDiagram.java
@@ -0,0 +1,20 @@
+﻿package LogicMath;
+
+import Other.P3WindowToViewport;
+import java.awt.*;
+import javax.swing.*;
+
+import LogicMath.discData.Bar;
+
+public class DrawDiagram extends JPanel{
+    private Bar[] bars;
+    private int width;
+    private int height;
+    private Bar b1;
+
+    
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        P3WindowToViewport converter = new P3WindowToViewport(null, width, height, width, height);
+    }
+}
---
+++ src/LogicMath/discData/Bar.java
@@ -0,0 +1,29 @@
+﻿package LogicMath.discData;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+public class Bar {
+
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+    private Color color;
+
+
+
+    public Bar(double x, double y, double width, double height, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+
+    public void drawBar(Graphics2D g) {
+        g.setColor(color);
+        g.fill(new Rectangle2D.Double(x, y, width, height));
+    }
+}
